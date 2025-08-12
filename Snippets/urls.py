07e6635167ab745181a7b9f128d6ea2f851e8ba7
kvepyt <kve@kve60.ru>
2025-08-12 06:17:09 +0300

    Update for local use

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from MainApp import views
 from django.contrib import admin
+from MainApp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("", views.index_page, name="home"),
     path("snippets/add", views.add_snippet_page, name="snippets-add"),
     path("snippets/list", views.snippets_page, name="snippets-list"),
