02b55dc6383e4183efe676be98ca0fd68e799396
kvepyt <kve@kve60.ru>
2025-08-13 18:02:13 +0300

    modify urlpatterns by use include (LOCAL)

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -1,22 +1,39 @@
-from django.urls import path
+from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 from MainApp import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("", views.index_page, name="home"),
-    path("snippets/add", views.add_snippet_page, name="snippets-add"),
-    path("snippets/list", views.snippets_page, name="snippets-list"),
-    path("snippets/my", views.my_snippets, name="my-snippets"),
-    path("snippets/<int:snippet_id>", views.snippet_detail, name="snippet-detail"),
-    path(
-        "snippets/<int:snippet_id>/delete", views.snippet_delete, name="snippet-delete"
-    ),
-    path("snippets/<int:snippet_id>/edit", views.snippet_edit, name="snippet-edit"),
+    path("admin/", admin.site.urls),
     path("login", views.login, name="login"),
     path("logout", views.logout, name="logout"),
     path("register", views.create_user, name="register"),
     path("comment/add", views.comment_add, name="comment_add"),
+    path(
+        "snippets/",
+        include(
+            [
+                path("add", views.add_snippet_page, name="snippets-add"),
+                path("list", views.snippets_page, name="snippets-list"),
+                path("my", views.my_snippets, name="my-snippets"),
+                path("<int:snippet_id>", views.snippet_detail, name="snippet-detail"),
+                path(
+                    "<int:snippet_id>/",
+                    include(
+                        [
+                            path("", views.snippet_detail, name="snippet-detail"),
+                            path(
+                                "delete",
+                                views.snippet_delete,
+                                name="snippet-delete",
+                            ),
+                            path("edit", views.snippet_edit, name="snippet-edit"),
+                        ]
+                    ),
+                ),
+            ]
+        ),
+    ),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
